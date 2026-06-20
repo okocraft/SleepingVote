4ac5f83ea838a1ac542edb5786ee559aeeec9a07
Siroshun09 <siro.shun8@gmail.com>
2026-06-20 17:21:47 +0900

    build: update dependencies and build logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,22 @@
 plugins {
-    java
-    id("com.github.johnrengelman.shadow") version "8.1.0"
+    alias(libs.plugins.jcommon)
+    alias(libs.plugins.bundler)
 }
 
 group = "net.okocraft.sleepingvote"
 version = "1.0"
 
-repositories {
-    mavenCentral()
-    maven(url = "https://repo.papermc.io/repository/maven-public/")
-}
+jcommon {
+    javaVersion = JavaVersion.VERSION_25
 
-dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    setupPaperRepository()
 
-    implementation("com.github.siroshun09.configapi:configapi-yaml:4.6.4")
-    implementation("com.github.siroshun09.translationloader:translationloader:2.0.2")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
-
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-
-tasks.build {
-    dependsOn(tasks.named("shadowJar"))
-}
-
-tasks.processResources {
-    filesMatching(listOf("plugin.yml", "en.yml", "ja_JP.yml")) {
-        expand("projectVersion" to version)
+    commonDependencies {
+        compileOnlyApi(libs.paper)
     }
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.shadowJar {
-    minimize()
-    relocate("com.github.siroshun09", "net.okocraft.boxtradestick.lib")
+bundler {
+    copyToRootBuildDirectory("SleepingVote-${project.version}")
+    replacePluginVersionForBukkit(project.version)
 }
